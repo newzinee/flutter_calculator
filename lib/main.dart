61b4e11819b1689450a5bd35d5f8b21c5d3ace08
treasureBear94 <qvo7896@gmail.com>
2021-01-10 22:33:11 +0900

    :sparkles: 마지막 글자 지우기, 사칙연산 중복 방지

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,42 @@ class StandardMode extends StatefulWidget {
 }
 
 class _StandardModeState extends State<StandardMode> {
-  var _result = '';
+  String _showText = '';
+  num _result;
 
-  void _inputText(var input) {
+  void _inputText(String input) {
     setState(() {
-      _result += input;
+      _showText += input;
+    });
+  }
+
+  void _inputNumber(num input) {
+    _inputText(input.toString());
+  }
+
+  void _inputRoles(String symbol) {
+    if (_showText == '') {
+    } else if (_showText.endsWith('+')) {
+    } else if (_showText.endsWith('-')) {
+    } else if (_showText.endsWith('%')) {
+    } else if (_showText.endsWith('/')) {
+    } else if (_showText.endsWith('x')) {
+    } else {
+      _inputText(symbol);
+    }
+  }
+
+  void _removeLast() {
+    if (_showText.isEmpty) return null;
+    setState(() {
+      _showText = _showText.substring(0, _showText.length - 1);
     });
   }
 
   void _clear() {
     setState(() {
-      _result = '';
+      _result = null;
+      _showText = '';
     });
   }
 
@@ -44,12 +69,32 @@ class _StandardModeState extends State<StandardMode> {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Container(
-          height: 100,
-          child: Text('$_result'),
+          padding: EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height * 20 / 100,
+          alignment: Alignment.centerRight,
+          child: Text(
+            '$_showText',
+            style: TextStyle(fontSize: 40),
+          ),
         ),
         Container(
           padding: EdgeInsets.all(10),
-          height: 500,
+          alignment: Alignment.centerRight,
+          child: Text(
+            '$_result',
+            style: TextStyle(fontSize: 20, color: Colors.grey),
+          ),
+        ),
+        Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.centerRight,
+            child: OutlineButton(
+              child: Icon(Icons.arrow_back),
+              onPressed: _removeLast,
+            )),
+        Container(
+          padding: EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height * 60 / 100,
           child: GridView.count(
             padding: const EdgeInsets.all(10),
             crossAxisCount: 4,
@@ -58,22 +103,22 @@ class _StandardModeState extends State<StandardMode> {
             children: [
               RaisedButton(onPressed: _clear, child: Text('C')),
               RaisedButton(onPressed: () {}, child: Text('()')),
-              RaisedButton(onPressed: () => _inputText('%'), child: Text('%')),
-              RaisedButton(onPressed: () => _inputText('/'), child: Text('/')),
-              RaisedButton(onPressed: () => _inputText('7'), child: Text('7')),
-              RaisedButton(onPressed: () => _inputText('8'), child: Text('8')),
-              RaisedButton(onPressed: () => _inputText('9'), child: Text('9')),
-              RaisedButton(onPressed: () => _inputText('x'), child: Text('x')),
-              RaisedButton(onPressed: () => _inputText('4'), child: Text('4')),
-              RaisedButton(onPressed: () => _inputText('5'), child: Text('5')),
-              RaisedButton(onPressed: () => _inputText('6'), child: Text('6')),
-              RaisedButton(onPressed: () => _inputText('-'), child: Text('-')),
-              RaisedButton(onPressed: () => _inputText('1'), child: Text('1')),
-              RaisedButton(onPressed: () => _inputText('2'), child: Text('2')),
-              RaisedButton(onPressed: () => _inputText('3'), child: Text('3')),
-              RaisedButton(onPressed: () => _inputText('+'), child: Text('+')),
+              RaisedButton(onPressed: () => _inputRoles('%'), child: Text('%')),
+              RaisedButton(onPressed: () => _inputRoles('/'), child: Text('/')),
+              RaisedButton(onPressed: () => _inputNumber(7), child: Text('7')),
+              RaisedButton(onPressed: () => _inputNumber(8), child: Text('8')),
+              RaisedButton(onPressed: () => _inputNumber(9), child: Text('9')),
+              RaisedButton(onPressed: () => _inputRoles('x'), child: Text('x')),
+              RaisedButton(onPressed: () => _inputNumber(4), child: Text('4')),
+              RaisedButton(onPressed: () => _inputNumber(5), child: Text('5')),
+              RaisedButton(onPressed: () => _inputNumber(6), child: Text('6')),
+              RaisedButton(onPressed: () => _inputRoles('-'), child: Text('-')),
+              RaisedButton(onPressed: () => _inputNumber(1), child: Text('1')),
+              RaisedButton(onPressed: () => _inputNumber(2), child: Text('2')),
+              RaisedButton(onPressed: () => _inputNumber(3), child: Text('3')),
+              RaisedButton(onPressed: () => _inputRoles('+'), child: Text('+')),
               RaisedButton(onPressed: () {}, child: Text('+/-')),
-              RaisedButton(onPressed: () => _inputText('0'), child: Text('0')),
+              RaisedButton(onPressed: () => _inputNumber(0), child: Text('0')),
               RaisedButton(onPressed: () {}, child: Text('.')),
               RaisedButton(onPressed: () {}, child: Text('=')),
             ],
